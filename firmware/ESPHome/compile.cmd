b0c4b3bece69ad31c4dadd0c2c68518e5bad0879
vzagorovskiy <vzagorovskiy@gmail.com>
2022-01-25 14:32:00 +0300

    Added script for adding ESPHome version to firmware binary name Updated firmware

--- firmware/ESPHome/compile.cmd
+++ firmware/ESPHome/compile.cmd
@@ -1,4 +1,4 @@
 call env.cmd
 call %ESPHOME_VENV%\Scripts\activate
 esphome compile yawl-controller-%1.yaml 
-xcopy /f /y ".esphome/build/yawl-controller/.pioenvs/yawl-controller/firmware.bin"  "yawl-controller-%1.bin"
+python copy_firmware.py %1
---
+++ firmware/ESPHome/copy_firmware.py
@@ -0,0 +1,24 @@
+import json
+import os
+import shutil
+import argparse
+
+DEVICE_NAME = "yawl-controller"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("firmware_id", type=str, help="Firmware ID")
+args = parser.parse_args()
+firmware_id = args.firmware_id
+
+
+
+PACKAGE_FILE = os.path.join(".esphome", f"{DEVICE_NAME}-{firmware_id}.yaml.json")
+
+with open(PACKAGE_FILE, "r") as package:
+    data = json.load(package)
+    esphome_version = data["esphome_version"]
+    firmware_bin_path =  data["firmware_bin_path"]
+    fname=f"{DEVICE_NAME}-{firmware_id}-{esphome_version}.bin" 
+    shutil.copy(firmware_bin_path, fname)
+    print(f"Copy firmware to {fname}")
+    
