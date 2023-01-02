9635bf2e435ede1f3a97f54f9a22ec9d39a1640b
vzagorovskiy <vzagorovskiy@gmail.com>
2023-01-02 15:42:18 +0300

    Updated WLED firmware to v0.14.0-b1

--- firmware/WLED/env.cmd
+++ firmware/WLED/env.cmd
@@ -1,7 +1,7 @@
-SET PYTHON_PATH=c:\Python\Python39
+SET PYTHON_PATH=c:\Python\Python310
 SET PLATFORMIO_VENV=c:\Python\Envs\platformio
 SET PLATFORMIO_REPO=https://github.com/platformio/platformio-core/
 SET PLATFORMIO_BRANCH=master
 SET WLED_REPO=https://github.com/Aircoookie/WLED
-SET WLED_BRANCH=v0.13.3
+SET WLED_BRANCH=v0.14.0-b1
 SET WLED_BUILD_DIR=.wled
--- firmware/WLED/install_platformio.cmd
+++ firmware/WLED/install_platformio.cmd
@@ -3,4 +3,5 @@ if exist %PLATFORMIO_VENV%\  rmdir /s /q %PLATFORMIO_VENV%
 %PYTHON_PATH%\python.exe -m venv %PLATFORMIO_VENV%
 call %PLATFORMIO_VENV%\Scripts\activate
 python.exe -m pip install --upgrade pip
-pip3 install --upgrade git+%PLATFORMIO_REPO%@%PLATFORMIO_BRANCH%
+pip install --upgrade wheel
+pip install --upgrade git+%PLATFORMIO_REPO%@%PLATFORMIO_BRANCH%
